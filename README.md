46ece24be7ec729350c7684083920a9d17f9880e
Rushikesh <rushibankhele@gmail.com>
2025-06-25 01:01:47 +0530

    Created A routing for application

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - remove unnecessary code and create a hello world app
 - install the tailwind and daisy UI for this application
 - added navbar to App.jsx
+- Install react-router-dom for routing purpose
+- create BrowserRoute > Routes > Route = /Body > RouteChildren
+- create an outlet in your body component 
+- create a footer
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <h1>This is a Devtinder app</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
