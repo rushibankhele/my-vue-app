ef76125c73d502f3990f7612b798f010b87b1422
Rushikesh <rushibankhele@gmail.com>
2025-06-24 15:29:51 +0530

    Create a vite+ react app

---
+++ README.md
@@ -0,0 +1,4 @@
+# DevTinder application
+
+- create vite + react application
+- remove unnecessary code and create a hello world app
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        this is vite-react app
+      </div> 
+    </>
+  )
+}
+
+export default App
